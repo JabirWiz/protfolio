d6b0a76cf4eb1975f6a213f1f4d9966e9078f270
Muhameed Jabir <mjabir.wiz@gmail.com>
2026-03-24 20:17:08 +0530

    new updates

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
   import { useMouseInElement, useResizeObserver } from "@vueuse/core";
-  import { reactive, ref, useTemplateRef, watch } from "vue";
+  import { onMounted, reactive, ref, useTemplateRef, watch } from "vue";
 
   import { useScroll } from "@vueuse/core";
   import Vue from "@/components/icons/vue.vue";
@@ -24,6 +24,7 @@
 
   const circleGrndt = useTemplateRef("circle-grdnt");
   const target = ref(null);
+  const section = ref("about");
   const body = document.getElementsByTagName("body")[0];
 
   console.log(body);
@@ -32,8 +33,8 @@
   const mouse = reactive(useMouseInElement(body));
 
   watch(mouse, () => {
-    console.log(window.scrollY);
-    console.log(mouse.x);
+    // console.log(window.scrollY);
+    // console.log(mouse.x);
     // console.log(mouse.y);
     // console.log(window.innerWidth);
     // console.log(isScrolling.value);
@@ -55,6 +56,34 @@
   window.addEventListener("scroll", (e) => {
     isScrolling.value = true;
   });
+
+  onMounted(() => {
+    const about: any = document.querySelector("#about-sec");
+    const experience: any = document.querySelector("#experience-sec");
+    const projects: any = document.querySelector("#projects-sec");
+    console.log("myDiv", about);
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            console.log("entry", entry.target.id);
+            section.value = entry.target.id;
+
+            console.log("The div is now visible!");
+            // You can trigger your scroll-based variable change here
+          } else {
+            console.log("The div is hidden.");
+          }
+        });
+      },
+      { threshold: 0.5 },
+    );
+
+    observer.observe(projects);
+    observer.observe(experience);
+    observer.observe(about);
+  });
 </script>
 
 <template>
@@ -73,9 +102,21 @@
               with precision and flair. -->
             </p>
 
-            <button class="text-left capitalize font-medium flex gap-4 items-center mt-10"><span class="border w-8 block h-0"></span> About</button>
-            <button class="text-left capitalize font-medium flex gap-4 items-center"><span class="border w-8 block h-0"></span> Experience</button>
-            <button class="text-left capitalize font-medium flex gap-4 items-center"><span class="border w-8 block h-0"></span> Projects</button>
+            <div class="hidden lg:flex gap-4 flex-col">
+              <!-- {{ section }} -->
+              <a href="#about" :class="{ 'opacity-50': section != 'about-sec' }" class="text-left uppercase font-medium text-sm flex gap-4 items-center mt-10">
+                <span :class="{ 'w-14 ': section == 'about-sec', 'w-8': section != 'about-sec' }" class="border block h-0 border-slate-400 transition-all"></span>
+                About
+              </a>
+              <a href="#experience" :class="{ 'opacity-50': section != 'experience-sec' }" class="text-left uppercase font-medium text-sm flex gap-4 items-center">
+                <span :class="{ 'w-14': section == 'experience-sec', 'w-8': section != 'experience-sec' }" class="border block h-0 border-slate-400 transition-all"></span>
+                Experience
+              </a>
+              <a href="#projects" :class="{ 'opacity-50': section != 'projects-sec' }" class="text-left uppercase font-medium text-sm flex gap-4 items-center">
+                <span :class="{ 'w-14': section == 'projects-sec', 'w-8': section != 'projects-sec' }" class="border block h-0 border-slate-400 transition-all"></span>
+                Projects
+              </a>
+            </div>
 
             <div class="grid-cols-3 w-max gap-x-12 gap-y-16 my-16 hidden">
               <Vue class="w-20 h-20" />
@@ -110,8 +151,8 @@
         </div>
 
         <div class="lg:w-1/2 lg:py-24 py-16">
-          <h4 class="font-Inter-bold text-xl text-white sticky top-0 lg:static py-4 bg-slate-900/50 backdrop-blur-sm lg:bg-transparent lg:backdrop-blur-none z-20 mb-2">About</h4>
-          <section class="text-slate-400 mb-24 lg:mb-32">
+          <h4 id="about" class="font-Inter-bold text-xl text-white sticky top-0 lg:static py-4 bg-slate-900/50 backdrop-blur-sm lg:bg-transparent lg:backdrop-blur-none z-20 mb-2">About</h4>
+          <section id="about-sec" class="text-slate-400 mb-24 lg:mb-32">
             <p class="mb-4">I'm a passionate Full-Stack Web Developer with a background in creating dynamic and scalable web applications. With experience in front-end frameworks like <a href="https://vuejs.org/" target="_blank">Vue</a> and <a href="https://nuxt.com/" target="_blank">Nuxt</a>, and back-end technologies like <a href="https://laravel.com/" target="_blank">Laravel</a> and <a href="https://firebase.google.com/" target="_blank">Firebase</a> , I enjoy turning complex problems into efficient solutions. I thrive in collaborative environments and have a strong focus on delivering high-quality, user-centric products.</p>
             <p class="mb-4">
               Throughout my career, I have had the opportunity to work on a variety of projects, from developing responsive e-commerce platforms to creating engaging and visually appealing websites. My experience with <a href="https://developer.mozilla.org/en-US/docs/Web/HTML" target="_blank">HTML</a>, <a href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="_blank">CSS</a>, <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">JavaScript</a>, <a href="https://www.typescriptlang.org/" target="_blank">TypeScript</a>, <a href="https://www.php.net/" target="_blank">PHP</a> and <a href="https://tailwindcss.com/" target="_blank">TailwindCSS</a>, enables me to deliver high-performance web solutions that prioritize both aesthetics and functionality. I believe
@@ -120,8 +161,8 @@
             <p class="mb-4">Outside of development, I enjoy reading and gaming, activities that help fuel my creativity and problem-solving skills. I am constantly seeking new challenges and opportunities to grow, both personally and professionally, and I am excited to bring my expertise and enthusiasm for web development to forward-thinking projects.</p>
           </section>
 
-          <h4 class="font-Inter-bold text-xl text-white sticky top-0 lg:static py-4 bg-slate-900/50 backdrop-blur-sm lg:bg-transparent lg:backdrop-blur-none z-20 mb-2">Experience</h4>
-          <section class="mb-24 lg:mb-32">
+          <h4 id="experience" class="font-Inter-bold text-xl text-white sticky top-0 lg:static py-4 bg-slate-900/50 backdrop-blur-sm lg:bg-transparent lg:backdrop-blur-none z-20 mb-2">Experience</h4>
+          <section id="experience-sec" class="mb-24 lg:mb-32">
             <ol>
               <li class="mb-12">
                 <a href="https://kbcinc.cloud/" target="_blank" class="grid gap-4 sm:grid-cols-8 group relative">
@@ -197,9 +238,9 @@
             </ol>
           </section>
 
-          <h4 class="font-Inter-bold text-xl text-white sticky top-0 lg:static py-4 bg-slate-900/50 backdrop-blur-sm lg:bg-transparent lg:backdrop-blur-none z-20">Projects</h4>
-          <p class="font-Inter-light text-slate-400 mb-14">Throughout my career, I have successfully delivered a variety of complex solutions. While much of my recent work involves proprietary internal platforms for private stakeholders, the following projects serve as a representative sample of my technical capabilities and architectural approach</p>
-          <section class="mb-24 lg:mb-32">
+          <h4 id="projects" class="font-Inter-bold text-xl text-white sticky top-0 lg:static py-4 bg-slate-900/50 backdrop-blur-sm lg:bg-transparent lg:backdrop-blur-none z-20">Projects</h4>
+          <section id="projects-sec" class="mb-24 lg:mb-32">
+            <p class="font-Inter-light text-slate-400 mb-14">Throughout my career, I have successfully delivered a variety of complex solutions. While much of my recent work involves proprietary internal platforms for private stakeholders, the following projects serve as a representative sample of my technical capabilities and architectural approach</p>
             <ol>
               <li class="mb-12">
                 <a href="https://www.archeme.com/" target="_blank" class="grid gap-6 sm:grid-cols-8 group relative">
@@ -239,6 +280,10 @@
 </template>
 
 <style>
+  html {
+    scroll-behavior: smooth;
+  }
+
   body {
     background: #0f172a;
     /* overflow: hidden; */
